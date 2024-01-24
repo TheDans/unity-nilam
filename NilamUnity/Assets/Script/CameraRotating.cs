607d7b450def321636e5faed7c31f2f7cb1c52d4
TheDans <danielharrith15@gmail.com>
2024-01-25 02:03:12 +0800

    adding EditMode
    adding edit mode as a scene

---
+++ NilamUnity/Assets/Script/CameraRotating.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CameraRotating : MonoBehaviour
+{
+    public float targetAngle = 45f;
+    public float currentAngle = 0f ;
+    public float rotationSpeed = 5f;
+    public float mouseSensitivity = 2f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if (Input.GetMouseButton(0))
+        {
+            targetAngle += mouseX * mouseSensitivity;
+
+        }
+        else {
+            targetAngle = Mathf.Round(targetAngle/45);
+            targetAngle *= 45;
+        }
+
+        if(targetAngle <0)
+        {
+            targetAngle += 360;
+        }
+
+        if(targetAngle > 360)
+        {
+            targetAngle -= 360;
+        }
+
+        currentAngle = Mathf.LerpAngle(transform.eulerAngles.y, targetAngle, rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(30, currentAngle, 0);
+    }
+}
